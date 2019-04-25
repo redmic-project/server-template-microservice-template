5d8b99752f95ca4f1ac70c3aa3a2f2ee43b56832
Noel <noel@redmic.es>
2019-04-25 09:46:51 +0000

    Añade config para exportar métricas

--- src/main/java/es/redmic/template/TemplateApplication.java
+++ src/main/java/es/redmic/template/TemplateApplication.java
@@ -1,14 +1,27 @@
 package es.redmic.template;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @ComponentScan({ "es.redmic.template" })
 public class TemplateApplication {
+    
+    @Value("${info.microservice.name}")
+	String microserviceName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateApplication.class, args);
+	}
+	
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", microserviceName);
 	}
 }
